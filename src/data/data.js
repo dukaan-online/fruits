4bc8379a146c5bc6bac51f32ebb523c15b23f355
mithu <mithu42329@gmail.com>
2023-06-10 00:32:33 +0530

    Adding fruits demo website

---
+++ src/data/data.js
@@ -0,0 +1,132 @@
+import apple from "../images/apple.jpg"
+import banana from "../images/banana.jpg"
+import anar from "../images/anar.jpg"
+import annanas from "../images/annanas.jpg"
+import grapes from "../images/grapes.jpg"
+import blackgrapes from "../images/black grapes.jpg"
+import kinu from "../images/kinu.jpg"
+import kiwi from "../images/kiwi.jpg"
+import orange from "../images/orange.jpg"
+import pineapple from "../images/pineapple.jpg"
+import redapple from "../images/red apple.jpg"
+import strawberry from "../images/strawberry.jpg"
+import tarbooj from "../images/tarbooj.jpg"
+import aadu from "../images/aadu.jpg"
+import mango from "../images/mango.jpg"
+
+
+
+export const MenuList = [
+    {
+        id: 1,
+        name: "Apple",
+        description: "500gm",
+        image: apple,
+        price: 60,
+    },
+    {
+        id: 2,
+        name: "Mango",
+        description: "500gm",
+        image: mango,
+        price: 80,
+        
+    },
+    {
+        id: 3,
+        name: "Banana",
+        description: "6 piece",
+        image: banana,
+        price: 30,
+        
+    },
+    {
+        id: 4,
+        name: "Red Apple",
+        description: "500gm",
+        image: redapple,
+        price: 80,
+    },
+    {
+        id: 5, 
+        name: "Kiwi",
+        description: "250gm",
+        image: kiwi,
+        price: 60,
+    },
+    {
+        id: 6,
+        name: "Grapes",
+        description: "250gm",
+        image: grapes,
+        price: 40,
+    },
+    {
+        id: 7,
+        name: "Black Grapes",
+        description: "250gm",
+        image: blackgrapes,
+        price: 40,
+    },
+    {
+        id: 8,
+        name: "Anar",
+        description: "500gm",
+        image: anar,
+        price: 70,
+    },
+    {
+        id: 9,
+        name: "Orange",
+        description: "500gm",
+        image: orange,
+        price: 60,
+    },
+    {
+        id: 10,
+        name: "Pineapple",
+        description: "1kg",
+        image: pineapple,
+        price: 100,
+    },
+    {
+        id: 11,
+        name: "Kinu",
+        description: "250gm",
+        image: kinu,
+        price: 50,
+    },
+    {
+        id: 12,
+        name: "Strawberry",
+        description: "200gm",
+        image: strawberry,
+        price: 40,
+    },
+    {
+        id: 13,
+        name: "Tarbooj",
+        description: "1kg",
+        image: tarbooj,
+        price: 60,
+    },
+    {
+        id: 14,
+        name: "Aadu",
+        description: "200gm",
+        image: aadu,
+        price: 30,
+    },
+    {
+        id: 15,
+        name: "Annanas",
+        description: "200gm",
+        image: annanas,
+        price: 40,
+    },
+];
+
+export const creds = {
+    username: "mohit",
+    password: "123"
+};
